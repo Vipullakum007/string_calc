3e9c240b5f0f8420cf1bd4dedeb9504f7ae6b8d8
Vipul Lakum <lakumvipul6351@gmail.com>
2025-07-03 22:27:52 +0530

    [Add step - 4] passed test : different delimiter(;)

--- src/main/java/org/vipul/StringCalculator.java
+++ src/main/java/org/vipul/StringCalculator.java
@@ -8,10 +8,25 @@ public class StringCalculator {
         if(input.isEmpty()){
             return 0;
         }
-        String[] numbers = input.split("[,\\n]");
         int sum = 0;
-        for(String number : numbers) {
-            sum += parseInt(number);
+        if(input.startsWith("//")){
+            int del_start_index = input.indexOf("//");
+            int del_end_index = input.indexOf("\n");
+
+            String delimiter = input.substring(del_start_index+2, del_end_index);
+
+            String org_string = input.substring(del_end_index+1, input.length());
+
+            String[] numbers = org_string.split(delimiter);
+            for(String number : numbers) {
+                sum += parseInt(number);
+            }
+        }
+        else {
+            String[] numbers = input.split("[,\\n]");
+            for (String number : numbers) {
+                sum += parseInt(number);
+            }
         }
         return sum;
     }
