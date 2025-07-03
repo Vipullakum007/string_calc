542958e5625f3edccd270ea7e98199f2401bae62
Vipul Lakum <lakumvipul6351@gmail.com>
2025-07-03 19:03:07 +0530

    [step - 1] added + passed basic test of empty string

--- src/main/java/org/vipul/StringCalculator.java
+++ src/main/java/org/vipul/StringCalculator.java
@@ -1,5 +1,8 @@
 package org.vipul;
 
-public class StringCalculator {
 
+public class StringCalculator {
+    public int add(String input) {
+        return 0;
+    }
 }
--- src/test/java/org/vipul/StringCalculatorTest.java
+++ src/test/java/org/vipul/StringCalculatorTest.java
@@ -1,4 +1,14 @@
 package org.vipul;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+    StringCalculator stringCalculator = new StringCalculator();
+
+
+    @Test
+    public void empty_string_should_return_0() {
+        assertEquals(0,stringCalculator.add(""));
+    }
 }
