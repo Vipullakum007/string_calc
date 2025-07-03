ea5fc6f438cbbb41e5e3f396061de457bf2be8f8
Vipul Lakum <lakumvipul6351@gmail.com>
2025-07-03 19:19:57 +0530

    [Add step - 1] added passed test : single number string should return that number

--- src/main/java/org/vipul/StringCalculator.java
+++ src/main/java/org/vipul/StringCalculator.java
@@ -1,10 +1,16 @@
 package org.vipul;
 
 
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
     public int add(String input) {
         if(input.isEmpty()){
             return 0;
+        }
+        String[] numbers = input.split(",");
+        if (numbers.length == 1) {
+            return parseInt(numbers[0]);
         }
         return 0;
     }
