acd436b1f5aa33d440a88129f3d08279a6e5ca0b
Vipul Lakum <lakumvipul6351@gmail.com>
2025-07-03 19:33:07 +0530

    [Add step - 2] added failed test : any amount numbers string should return sum of all those numbers

--- src/test/java/org/vipul/StringCalculatorTest.java
+++ src/test/java/org/vipul/StringCalculatorTest.java
@@ -23,4 +23,12 @@ public class StringCalculatorTest {
         assertEquals(7,stringCalculator.add("3,4"));
         assertEquals(50,stringCalculator.add("10,40"));
     }
+
+    @Test
+    public void any_amount_numbers_string_should_return_sum_of_all_those_numbers(){
+        assertEquals(6,stringCalculator.add("2,4"));
+        assertEquals(12,stringCalculator.add("2,4,6"));
+        assertEquals(10,stringCalculator.add("1,2,3,4"));
+        assertEquals(55,stringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
+    }
 }
